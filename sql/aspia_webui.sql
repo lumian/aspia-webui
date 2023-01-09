5bbe2ebc60f0c2bb8f8ddb2922e1275376bf34b8
lumian <lum@it-35.ru>
2023-01-09 11:26:34 +0300

    Add SQL template

---
+++ sql/aspia_webui.sql
@@ -0,0 +1,66 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               10.3.36-MariaDB-0+deb10u2 - Debian 10
+-- Операционная система:         debian-linux-gnu
+-- HeidiSQL Версия:              12.2.0.6576
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Дамп структуры для таблица kc_aspia_new.installers_data
+CREATE TABLE IF NOT EXISTS `installers_data` (
+  `installer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `installer_name` varchar(50) DEFAULT NULL,
+  `installer_description` varchar(250) DEFAULT NULL,
+  `installer_url` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`installer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица kc_aspia_new.packages_data
+CREATE TABLE IF NOT EXISTS `packages_data` (
+  `package_id` int(11) NOT NULL AUTO_INCREMENT,
+  `package_name` varchar(50) DEFAULT NULL,
+  `package_description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`package_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица kc_aspia_new.sessions
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT 0,
+  `data` blob NOT NULL,
+  KEY `ci_sessions_timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица kc_aspia_new.updates_data
+CREATE TABLE IF NOT EXISTS `updates_data` (
+  `update_id` int(11) NOT NULL AUTO_INCREMENT,
+  `package_id` int(11) DEFAULT NULL,
+  `update_source_version` varchar(50) DEFAULT NULL,
+  `update_target_version` varchar(50) DEFAULT NULL,
+  `update_description` text DEFAULT NULL,
+  `installer_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`update_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Экспортируемые данные не выделены.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
