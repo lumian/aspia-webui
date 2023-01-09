0d0ee278884d7495de84050e26f01bda521f6cad
lumian <lum@it-35.ru>
2023-01-09 16:35:07 +0300

    Update base_template.php

--- upload/application/views/base_template.php
+++ upload/application/views/base_template.php
@@ -74,7 +74,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<!-- Fixed navbar -->
 		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="#"><?=$this->config->item('site_title', 'aspia');?></a>
+				<a class="navbar-brand" href="<?=base_url();?>"><?=$this->config->item('site_title', 'aspia');?></a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
